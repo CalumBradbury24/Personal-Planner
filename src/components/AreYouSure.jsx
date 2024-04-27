d028457ea3f970362cf84b9aacdd60ce29dbb04b
Calum Bradbury <calumbradbury@Calums-Laptop.local>
2024-04-27 14:15:45 +0100

    add service worker config

--- src/components/AreYouSure.jsx
+++ src/components/AreYouSure.jsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
+import { devices } from "../styles/styleConstants";
 
 const StyledP = styled.p`
   color: var(--color-grey-900);
 `;
 
+const StyledDiv = styled.div`
+  @media screen and ${devices.medium} {
+    font-size: 16px;
+  }
+`;
+
 function AreYouSure({ text, children }) {
   return (
-    <div>
+    <StyledDiv>
       <StyledP>{text}</StyledP>
       {children}
-    </div>
+    </StyledDiv>
   );
 }
 
